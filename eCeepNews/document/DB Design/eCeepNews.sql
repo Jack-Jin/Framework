f96586a439606c9f560487ce6141bf00494d2591
Jack <jackjin.com@gmail.com>
2015-06-02 17:17:55 -0400

    sql script little change.

--- eCeepNews/document/DB Design/eCeepNews.sql
+++ eCeepNews/document/DB Design/eCeepNews.sql
@@ -3,3 +3,6 @@ SHOW TABLES;
 SELECT ID,Title,Content,Active,CreatedByID,CreatedByName,CreatedTime,ModifiedByID,ModifiedByName,ModifiedTime FROM News;
 
 SELECT ID,Title,Content,Active,CreatedByID,CreatedByName,CreatedTime,ModifiedByID,ModifiedByName,ModifiedTime FROM News ORDER BY ModifiedTime DESC
+
+
+
